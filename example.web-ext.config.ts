a7fa9c3b74c49d956b1228df039ceb67cdc6d45f
kaznak <kaznak.at.work@ipebble.org>
2025-03-05 13:11:23 +0000

    > npm run format

--- example.web-ext.config.ts
+++ example.web-ext.config.ts
@@ -1,9 +1,8 @@
-import { defineRunnerConfig } from 'wxt';
+import { defineRunnerConfig } from "wxt";
 
 // dev サーバ起動時のブラウザ立ち上げを無効化する設定です。
 // この設定を使用する場合はこのファイルを web-ext.config.ts にリネームしてください。
 
 export default defineRunnerConfig({
-    disabled: true,
-  });
-  
+  disabled: true,
+});
